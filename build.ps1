a16c6822b4a65bdc4921a1b20c9af56303df9f35
Bruce Wayne <admin@bige0.com>
2020-08-24 21:45:14 +0800

    Add build script

---
+++ build.ps1
@@ -0,0 +1,69 @@
+param([string]$buildtfm = 'all')
+$ErrorActionPreference = 'Stop'
+
+Write-Host 'dotnet SDK version'
+dotnet --version
+
+$exe = 'BilibiliLiveRecordDownLoader.exe'
+$netcore_tfm = 'netcoreapp3.1'
+$configuration = 'Release'
+$output_dir = "BilibiliLiveRecordDownLoader\bin\$configuration"
+$dllpatcher_dir = 'Build\DotNetDllPathPatcher'
+$proj_path = 'BilibiliLiveRecordDownLoader\BilibiliLiveRecordDownLoader.csproj'
+
+$buildCore    = $buildtfm -eq 'all' -or $buildtfm -eq 'core'
+$buildCoreX86 = $buildtfm -eq 'all' -or $buildtfm -eq 'core-x86'
+$buildCoreX64 = $buildtfm -eq 'all' -or $buildtfm -eq 'core-x64'
+
+function Build-NetCore
+{
+    Write-Host 'Building .NET Core'
+
+    $outdir = "$output_dir\$netcore_tfm"
+    $publishDir = "$outdir\publish"
+
+    Remove-Item $publishDir -Recurse -Force -Confirm:$false -ErrorAction Ignore
+
+    dotnet publish -c $configuration -f $netcore_tfm $proj_path
+    if ($LASTEXITCODE) { exit $LASTEXITCODE }
+
+    & $dllpatcher_dir\bin\$configuration\$netcore_tfm\DotNetDllPathPatcher.exe $publishDir\$exe bin
+    if ($LASTEXITCODE) { exit $LASTEXITCODE }
+}
+
+function Build-NetCoreSelfContained
+{
+    param([string]$arch)
+
+    Write-Host "Building .NET Core $arch"
+
+    $rid = "win-$arch"
+    $outdir = "$output_dir\$netcore_tfm\$rid"
+    $publishDir = "$outdir\publish"
+
+    Remove-Item $publishDir -Recurse -Force -Confirm:$false -ErrorAction Ignore
+
+    dotnet publish -c $configuration -f $netcore_tfm -r $rid --self-contained true $proj_path
+    if ($LASTEXITCODE) { exit $LASTEXITCODE }
+
+    & $dllpatcher_dir\bin\$configuration\$netcore_tfm\DotNetDllPathPatcher.exe $publishDir\$exe bin
+    if ($LASTEXITCODE) { exit $LASTEXITCODE }
+}
+
+dotnet build -c $configuration -f $netcore_tfm $dllpatcher_dir\DotNetDllPathPatcher.csproj
+if ($LASTEXITCODE) { exit $LASTEXITCODE }
+
+if ($buildCore)
+{
+    Build-NetCore
+}
+
+if ($buildCoreX64)
+{
+    Build-NetCoreSelfContained x64
+}
+
+if ($buildCoreX86)
+{
+    Build-NetCoreSelfContained x86
+}
