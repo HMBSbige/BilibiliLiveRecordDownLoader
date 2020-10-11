eb14847d5bde29570696d96663da22a2308ecfc2
Bruce Wayne <admin@bige0.com>
2020-10-11 16:15:28 +0800

    Update build script and CI

--- Build/GetSHA256.ps1
+++
@@ -1,6 +0,0 @@
-param([string]$file)
-$hash = [Security.Cryptography.HashAlgorithm]::Create( "SHA256" )
-$path = (Resolve-Path -Path $file).Path
-$stream = ([IO.StreamReader]$path).BaseStream
--join ($hash.ComputeHash($stream) | ForEach-Object { "{0:X2}" -f $_ })
-$stream.Close()
