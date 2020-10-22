6fba23a9dfd7ea3367099b7ed71e164bf7448522
Bruce Wayne <admin@bige0.com>
2020-10-22 13:37:14 +0800

    [WIP] New MultiThreadedDownloader

---
+++ BilibiliLiveRecordDownLoader.Http/DownLoaders/FileRange.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Http.Headers;
+
+namespace BilibiliLiveRecordDownLoader.Http.DownLoaders
+{
+    public class FileRange
+    {
+        public RangeHeaderValue Range { get; set; }
+        public string FileName { get; set; }
+    }
+}
--- BilibiliLiveRecordDownLoader.Http/DownLoaders/MultiThreadedDownloader.cs
+++ BilibiliLiveRecordDownLoader.Http/DownLoaders/MultiThreadedDownloader.cs
@@ -1,10 +1,13 @@
 ﻿using BilibiliLiveRecordDownLoader.Http.HttpPolicy;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.ObjectPool;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using System.Threading;
@@ -14,6 +17,8 @@ namespace BilibiliLiveRecordDownLoader.Http.DownLoaders
 {
     public class MultiThreadedDownloader : IDownloader
     {
+        private readonly ILogger _logger;
+
         private readonly BehaviorSubject<double> _progressUpdated = new BehaviorSubject<double>(0.0);
         public IObservable<double> ProgressUpdated => _progressUpdated.AsObservable();
 
@@ -34,9 +39,18 @@ namespace BilibiliLiveRecordDownLoader.Http.DownLoaders
 
         private readonly ObjectPool<HttpClient> _httpClientPool;
 
-        public MultiThreadedDownloader()
+        static MultiThreadedDownloader()
         {
-            ServicePointManager.DefaultConnectionLimit = 10000;
+            const int connectionLimit = 10000;
+            if (ServicePointManager.DefaultConnectionLimit < connectionLimit)
+            {
+                ServicePointManager.DefaultConnectionLimit = connectionLimit;
+            }
+        }
+
+        public MultiThreadedDownloader(ILogger logger)
+        {
+            _logger = logger;
 
             var policy = new PooledHttpClientPolicy(CreateNewClient);
             var provider = new DefaultObjectPoolProvider { MaximumRetained = 10 };
@@ -70,7 +84,7 @@ namespace BilibiliLiveRecordDownLoader.Http.DownLoaders
         /// </summary>
         /// <param name="token"></param>
         /// <returns></returns>
-        private async Task<long> GetContentLengthAsync(CancellationToken token)
+        private async ValueTask<long> GetContentLengthAsync(CancellationToken token)
         {
             var client = _httpClientPool.Get();
             try
@@ -88,9 +102,54 @@ namespace BilibiliLiveRecordDownLoader.Http.DownLoaders
             }
         }
 
-        public ValueTask DownloadAsync(CancellationToken token)
+        public async ValueTask DownloadAsync(CancellationToken token)
         {
-            throw new NotImplementedException();
+            var length = await GetContentLengthAsync(token); //总大小
+
+            TempDir = EnsureDirectory(TempDir);
+            var list = GetFileRangeList(length);
+
+
+        }
+
+        private static string EnsureDirectory(string path)
+        {
+            try
+            {
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                return path;
+            }
+            catch
+            {
+                return Directory.GetCurrentDirectory();
+            }
+        }
+
+        private string GetTempFileName() => Path.Combine(TempDir, Path.GetRandomFileName());
+
+        private IEnumerable<FileRange> GetFileRangeList(long length)
+        {
+            var list = new List<FileRange>();
+
+            var parts = Threads; //线程数
+            var partSize = length / parts; //每块大小
+
+            _logger.LogDebug($@"总大小：{length} ({Target})");
+            _logger.LogDebug($@"每块大小：{partSize} ({Target})");
+
+            for (var i = 1; i < parts; ++i)
+            {
+                var range = new RangeHeaderValue((i - 1) * partSize, i * partSize);
+                list.Add(new FileRange { FileName = GetTempFileName(), Range = range });
+            }
+
+            var last = new RangeHeaderValue((parts - 1) * partSize, length);
+            list.Add(new FileRange { FileName = GetTempFileName(), Range = last });
+
+            return list;
         }
 
         public ValueTask DisposeAsync()
