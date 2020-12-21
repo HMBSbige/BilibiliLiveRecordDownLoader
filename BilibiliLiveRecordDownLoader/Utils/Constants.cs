396b38e45706abe7e4da6862fc7d4410918bf149
Bruce Wayne <admin@bige0.com>
2020-12-21 17:13:57 +0800

    Change colors

--- BilibiliLiveRecordDownLoader/Utils/Constants.cs
+++ BilibiliLiveRecordDownLoader/Utils/Constants.cs
@@ -14,10 +14,11 @@ namespace BilibiliLiveRecordDownLoader.Utils
 		public const string FFmpegCopyConvert = @"-i ""{0}"" -c:v copy -c:a copy -y ""{1}""";
 		public const string FFmpegSplitTo = @"-ss {0} -to {1} -accurate_seek -i ""{2}"" -codec copy -avoid_negative_ts 1 ""{3}"" -y";
 
+		public static readonly SolidColorBrush NormalBlueBrush = new(Color.FromRgb(38, 160, 218));
 		public static readonly SolidColorBrush RedBrush = new(Colors.Red);
-		public static readonly SolidColorBrush GreenBrush = new(Colors.Green);
+		public static readonly SolidColorBrush GreenBrush = NormalBlueBrush;
 		public static readonly SolidColorBrush YellowBrush = new(Colors.Coral);
-		public static readonly SolidColorBrush NormalDiskUsageBrush = new(Color.FromRgb(38, 160, 218));
+		public static readonly SolidColorBrush NormalDiskUsageBrush = NormalBlueBrush;
 
 		public static readonly CommonFileDialogFilter VideoFilter = new(@"视频文件", @"*.mp4;*.flv;*.mkv");
 		public static readonly CommonFileDialogFilter AllFilter = new(@"所有文件", @"*.*");
