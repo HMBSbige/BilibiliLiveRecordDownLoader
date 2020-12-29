dfc3c9399525988650978c6a419db09ed29083ef
Bruce Wayne <admin@bige0.com>
2020-12-29 16:50:32 +0800

    [WIP] Add api: manga

---
+++ BilibiliLiveRecordDownLoader.BilibiliApi/Model/Manga/GetClockInInfo/GetClockInInfoMessage.cs
@@ -0,0 +1,20 @@
+namespace BilibiliApi.Model.Manga.GetClockInInfo
+{
+	public class GetClockInInfoMessage
+	{
+		/// <summary>
+		/// 正常为 0
+		/// </summary>
+		public long code { get; set; }
+
+		/// <summary>
+		/// 消息
+		/// </summary>
+		public string? msg { get; set; }
+
+		/// <summary>
+		/// 漫画签到信息
+		/// </summary>
+		public GetClockInInfoData? data { get; set; }
+	}
+}
---
+++ UnitTest/TestConstants.cs
@@ -0,0 +1,11 @@
+namespace UnitTest
+{
+	public static class TestConstants
+	{
+		public const string Cookie = @"";
+		public const string Username = @"";
+		public const string Password = @"";
+		public const string AccessToken = @"";
+		public const string RefreshToken = @"";
+	}
+}
