363e6c06ad44eb25fbdedf55c19513c6d8fb1372
Bruce Wayne <admin@bige0.com>
2021-04-01 09:24:54 +0800

    Clean code

--- BilibiliLiveRecordDownLoader.Shared/Utils/Extensions.cs
+++ BilibiliLiveRecordDownLoader.Shared/Utils/Extensions.cs
@@ -14,28 +14,6 @@ namespace BilibiliLiveRecordDownLoader.Shared.Utils
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static void NoWarning(this ValueTask _) { }
 
-		public static int GetDeterministicHashCode(this string str)
-		{
-			unchecked
-			{
-				var hash1 = (5381 << 16) + 5381;
-				var hash2 = hash1;
-
-				for (var i = 0; i < str.Length; i += 2)
-				{
-					hash1 = ((hash1 << 5) + hash1) ^ str[i];
-					if (i == str.Length - 1)
-					{
-						break;
-					}
-
-					hash2 = ((hash2 << 5) + hash2) ^ str[i + 1];
-				}
-
-				return hash1 + hash2 * 1566083941;
-			}
-		}
-
 		public static string ToCookie(this IEnumerable<string> cookies)
 		{
 			var hashSet = new HashSet<string>();
