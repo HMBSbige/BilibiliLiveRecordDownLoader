a0d0b4bbb6f837d05282f37b9b35667909de9540
Bruce Wayne <admin@bige0.com>
2021-06-18 15:15:58 +0800

    Update UA

--- BilibiliLiveRecordDownLoader.Shared/Utils/UserAgents.cs
+++ BilibiliLiveRecordDownLoader.Shared/Utils/UserAgents.cs
@@ -3,7 +3,7 @@ namespace BilibiliLiveRecordDownLoader.Shared.Utils
 	public static class UserAgents
 	{
 		public const string Idm = @"Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko";
-		public const string Chrome = @"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36";
-		public const string BilibiliManga = @"Mozilla/5.0 BiliComic/3.8.0";
+		public const string Chrome = @"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36";
+		public const string BilibiliManga = @"Mozilla/5.0 BiliComic/3.15.2";
 	}
 }
